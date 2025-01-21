e4625748e72c51d135a2518c3c12df4956a7a31f
Shigabutdinoff <shigabutdinovbr@gmail.com>
2025-01-21 22:39:32 +0300

    Initial AdminPanel.vue

---
+++ src/components/AdminPanel.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import ShowUsers from "@/components/ShowUsers.vue";
+</script>
+
+<template>
+  <ShowUsers/>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import AdminPanel from '../components/AdminPanel.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <AdminPanel />
   </main>
 </template>
